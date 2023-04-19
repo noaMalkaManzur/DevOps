d84400279665a32220e49ad8046a89e6ddb3ba1a
noaMalkaManzur <noamanzur1998@gmail.com>
2023-04-19 17:36:37 +0300

    changing another print

--- NoaMalkaManzur.java
+++ NoaMalkaManzur.java
@@ -2,7 +2,7 @@ public class PrintMyName{
 
 	public static void main(String[] args) {
 		for (int i=1;i<=10; i++) {
-			System.out.println("Noa malka Manzur ..."+i);
+			System.out.println("print after change ..."+i);
 		}
 	}
 }
