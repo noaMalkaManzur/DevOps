e8c28daec64b809467ff413a9be88d4e6d8466c0
noaMalkaManzur <noamanzur1998@gmail.com>
2023-04-21 17:31:15 +0300

    adding files to git

--- NoaMalkaManzur.java
+++ NoaMalkaManzur.java
@@ -2,7 +2,7 @@ public class PrintMyName{
 
 	public static void main(String[] args) {
 		for (int i=1;i<=10; i++) {
-			System.out.println("print after change ..."+i);
+			System.out.println("noa malka manzur ..."+i);
 		}
 	}
 }
